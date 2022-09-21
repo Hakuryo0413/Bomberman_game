9495fab2d5e0f2476dd58396d403a3c449f67d5a
Hakuryo0413 <100341996+Hakuryo0413@users.noreply.github.com>
2022-09-21 23:55:07 +0700

    entities1

--- src/main/java/com/g10/entities/Entities.java
+++ src/main/java/com/g10/entities/Entities.java
@@ -1,5 +1,34 @@
 package com.g10.entities;
 
 public class Entities {
-    //Trammm
+    private int x;
+    private int y;
+    private String path;
+
+    public Entities() {
+    }
+
+    public int getXEntities() {
+        return x;
+    }
+
+    public int getYEntities() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getPathToImage() {
+        return path;
+    }
+
+    public void setPathToImage(String path) {
+        this.path = path;
+    }
 }
