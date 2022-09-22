7b4aadfdfc504aa23d76f5841360783c20ba86a9
kimchiiiiii <21020287@vnu.edu.vn>
2022-09-22 13:51:58 +0700

    renderer

--- src/main/java/com/g10/renderer/Renderer.java
+++ src/main/java/com/g10/renderer/Renderer.java
@@ -1,5 +1,13 @@
 package com.g10.renderer;
 
+import com.g10.img.ImageManager;
+import com.g10.entities.Entities;
+import com.g10.sandbox.Sandbox;
+import javafx.scene.image.Image;
+
 public class Renderer {
-    //Chi
+    public static void renderImage(Entities entity) {
+        Image img = ImageManager.getImage(entity.getPathToImage());
+        Sandbox.getGc().drawImage(img, entity.getXEntities(), entity.getYEntities());
+    }
 }
