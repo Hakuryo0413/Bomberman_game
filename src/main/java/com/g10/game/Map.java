3a593807395eb560be4ac1042c00ff6f8b8773a9
Hakuryo0413 <100341996+Hakuryo0413@users.noreply.github.com>
2022-10-01 01:25:50 +0700

    updateMap1

--- src/main/java/com/g10/game/Map.java
+++ src/main/java/com/g10/game/Map.java
@@ -1,5 +1,3 @@
-package com.g10.game;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
@@ -9,7 +7,7 @@ import java.util.Scanner;
 public class Map {
     private int width;
     private int height;
-    private final int[][] arr = new int[width][height];
+    private int[][] a;
 
     private int getHeight() {
         return height;
@@ -27,21 +25,27 @@ public class Map {
         this.width = width;
     }
 
-    public int[][] LoadMap(String file) {
+    private void setArray(int[][] a) {
+        this.a = a;
+    }
+
+    public void LoadMap(String file) {
         try {
-            File f = new File("file.txt");
+            File f = new File(file);
             Scanner s1 = new Scanner(f);
             width = s1.nextInt();
             height = s1.nextInt();
+            a = new int[width][height];
             for (int i = 0; i < width; i++) {
                 for (int j = 0; j < height; j++) {
-                    arr[i][j] = s1.nextInt();
+                    a[i][j] = s1.nextInt();
                 }
             }
-            return arr;
+            s1.close();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
 }
+
 
