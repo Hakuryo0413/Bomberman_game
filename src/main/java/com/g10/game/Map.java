4d69386ab2631c1bb9d02543ef1ad485d700d264
Hakuryo0413 <100341996+Hakuryo0413@users.noreply.github.com>
2022-10-01 03:19:27 +0700

    render

--- src/main/java/com/g10/game/Map.java
+++ src/main/java/com/g10/game/Map.java
@@ -1,15 +1,39 @@
 package com.g10.game;
 
+import java.awt.*;
+import java.awt.image.ImageObserver;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 import java.util.Scanner;
+import java.awt.Image;
+import com.g10.constants.GlobalConstant;
+//import javafx.scene.image.Image;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
 
 
 public class Map {
     private int width;
     private int height;
+    private Image image;
     private int[][] a;
+
+    public void render() {
+
+    }
+    public void render(String path, Image image){
+
+    }
+    public void render(String path,Image image,Graphics g){
+        Toolkit t=Toolkit.getDefaultToolkit();
+        image = t.getImage(path);
+        g.drawImage(image,0,0, width *GlobalConstant.TILE_SIZE,height *GlobalConstant.TILE_SIZE,null);
+    }
+
+
+
+
 
     public int getHeight() {
         return height;
@@ -25,6 +49,12 @@ public class Map {
 
     public void setWidth(int width) {
         this.width = width;
+    }
+    public void setImage(Image image){
+        this.image=image;
+    }
+    public Image getImage(){
+        return image;
     }
 
     public void setArray(int[][] a) {
