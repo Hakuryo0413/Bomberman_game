b6327edcacabd51ea0d60bf7f7925e0da3113c65
Hakuryo0413 <100341996+Hakuryo0413@users.noreply.github.com>
2022-10-21 17:09:13 +0700

    CreateHome

--- src/main/java/com/g10/menu/GameMenu.java
+++ src/main/java/com/g10/menu/GameMenu.java
@@ -2,6 +2,8 @@ package com.g10.menu;
 
 import com.g10.constants.GlobalConstant;
 import com.g10.general.*;
+import com.g10.screens.ScreenManager;
+import com.g10.screens.ScreenType;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -29,8 +31,8 @@ public class GameMenu {
         } catch (FileNotFoundException e) {
             System.out.println("not found");
         }
-        cursor = new Cursor(ImageManager.getImage("asset/menu/Cursor_Pause.png"), 280, 230);
-        cursor.setCount(2);
+        cursor = new Cursor(ImageManager.getImage("asset/menu/Cursor_Pause.png"), 280, 250);
+        cursor.setCount(3);
         cursor.setSpaceBetween(20);
     }
 
@@ -57,6 +59,10 @@ public class GameMenu {
                 if (cursor.getNumOfSelect() == 2) {
                     AudioManager.switchMuteSound();
                 }
+                if (cursor.getNumOfSelect() == 3) {
+                    ScreenManager.switchScreen(ScreenType.HOME_SCREEN);
+                }
+
             }
         }
     }
@@ -64,7 +70,6 @@ public class GameMenu {
     public void render() {
         if (active) {
             Sandbox.getGc().setFill(Color.BLACK);
-
             Sandbox.getGc().setGlobalAlpha(0.6);
             Sandbox.getGc().fillRect(0, 0, GlobalConstant.SCREEN_WIDTH, GlobalConstant.SCREEN_HEIGHT);
             Sandbox.getGc().setGlobalAlpha(1);
@@ -72,15 +77,17 @@ public class GameMenu {
             Sandbox.getGc().setFont(font);
             Sandbox.getGc().setFill(Color.WHITE);
             if (AudioManager.isMuteMusic()) {
-                Sandbox.getGc().fillText("MUSIC: OFF", cursor.getX() + 65, cursor.getY() + 35);
+                Sandbox.getGc().fillText("MUSIC: OFF", cursor.getX() + 50, cursor.getY() + 35);
             } else {
-                Sandbox.getGc().fillText("MUSIC: ON", cursor.getX() + 65, cursor.getY() + 35);
+                Sandbox.getGc().fillText("MUSIC: ON", cursor.getX() + 50, cursor.getY() + 35);
             }
             if (AudioManager.isMuteSound()) {
-                Sandbox.getGc().fillText("SOUND: OFF", cursor.getX() + 65, cursor.getY() + 35 + cursor.getSpaceBetween() + GlobalConstant.TILE_SIZE);
+                Sandbox.getGc().fillText("SOUND: OFF", cursor.getX() + 50, cursor.getY() + 35 + cursor.getSpaceBetween() + GlobalConstant.TILE_SIZE);
             } else {
-                Sandbox.getGc().fillText("SOUND: ON", cursor.getX() + 65, cursor.getY() + 35 + cursor.getSpaceBetween() + GlobalConstant.TILE_SIZE);
+                Sandbox.getGc().fillText("SOUND: ON", cursor.getX() + 50, cursor.getY() + 35 + cursor.getSpaceBetween() + GlobalConstant.TILE_SIZE);
             }
+            Sandbox.getGc().fillText("HOME", cursor.getX() + 50, cursor.getY() + 35 + 2*(cursor.getSpaceBetween() + GlobalConstant.TILE_SIZE));
+
         }
     }
 
